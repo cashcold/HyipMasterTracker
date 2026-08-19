3b4fe786f6ba4287696cf9aab60332a572a2a3e7
Frank99 <frankainoo@gmail.com>
2026-08-19 15:50:28 +0000

    Clean project files and update components

--- src/components/others/others.js
+++
@@ -1,7 +0,0 @@
-// MONGODB_URI=mongodb+srv://nextplatform:henry8messi@nextplatform.fbj1o.mongodb.net/?appName=NextPlatform
-// JWT_SECRET=goldbod_pro_super_secret_jwt_key_2026
-// SMTP_HOST=smtp-relay.brevo.com
-// SMTP_PORT=587
-// SMTP_USER=b607a2001@smtp-brevo.com
-// SMTP_PASS=xsmtpsib-56a8f27fb389d51661373a9348469bb08f0f9f7a98a51724d0643f1b15e7952e-EOLIscaouvUovpZQ
-// SENDER_EMAIL=cashcold99@gmail.com
